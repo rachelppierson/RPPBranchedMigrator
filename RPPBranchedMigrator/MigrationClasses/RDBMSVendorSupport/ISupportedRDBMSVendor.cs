971443a131262f2405a6cc6cd6eb0f88b87beb48
Rachel Pierson <not@allinterestedinthis.com>
2013-06-29 21:04:43 +0100

    Adding new files missed by previous Commit.

---
+++ RPPBranchedMigrator/MigrationClasses/RDBMSVendorSupport/ISupportedRDBMSVendor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MigrationClasses.RDBMSVendorSupport
+{
+    /// <summary>
+    /// Minimum functionality that must be supported by any RDBMS targeted by BranchedMigrator
+    /// </summary>
+    public interface ISupportedRDBMSVendor
+    {
+        /// <summary>
+        /// Contains the connection string appropriate to the RDBMS targeted. Must be set for most methods
+        /// in the Class to be able to be run successfully.
+        /// </summary>
+        string ConnectionString { get; set; }
+
+        /// <summary>Attempts to begin a Transaction within the context of the RDBMS targeted and ConnectionString set</summary>
+        void BeginTransaction();
+
+        /// <summary>Attempts to commit a Transaction within the context of the RDBMS targeted and ConnectionString set</summary>
+        void CommitTransaction();
+
+        /// <summary>Attempts to commit a Transaction within the context of the RDBMS targeted and ConnectionString set</summary>
+        void RollbackTransaction();
+ 
+        /// <summary>Attempts to run SQL commands contained in a .SQL file</summary>
+        /// <param name="sqlFilePath">Fully-qualified path to a file containing valid SQL</param>
+        void RunCommandsInSQLFile(string sqlFilePath);
+    }
+}
---
+++ RPPBranchedMigrator/MigrationClasses/RDBMSVendorSupport/VendorSpecificImplementations/SQLServer.cs
@@ -0,0 +1,64 @@
+﻿
+#region Using Directives
+
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+#endregion
+
+namespace MigrationClasses.RDBMSVendorSupport.VendorSpecificImplementations
+{
+    public class SQLServer : ISupportedRDBMSVendor
+    {
+        #region Fields
+
+        SqlConnection connection = null;
+
+        SqlTransaction transaction = null;
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+
+        #region ISupportedRDBMSVendor implementation
+
+        public string ConnectionString { get; set; }
+
+        public void BeginTransaction()
+        {
+            if (!(connection is SqlConnection)) throw new SqlException(@"");
+            if (transaction is SqlTransaction) throw new SqlException(@"");
+            if (connection.State == ConnectionState.Closed) connection.Open();
+            transaction = connection.BeginTransaction();
+        }
+
+        public void CommitTransaction()
+        {
+            if (!(connection is SqlConnection) || !(transaction is SqlTransaction)) return false;
+            transaction.Commit();
+        }
+
+        public void RollbackTransaction()
+        {
+            if (!(connection is SqlConnection) || !(transaction is SqlTransaction)) return false;
+            transaction.Rollback();
+        }
+
+        public Exception LastErrorDetails { get; set; }
+
+        public void RunCommandsInSQLFile(string sqlFilePath)
+        {
+
+        }
+
+        #endregion
+    }
+}

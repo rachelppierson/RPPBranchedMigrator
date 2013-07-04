07c619bcd0c1461009d2be749ea35c13f2ed8e5c
Rachel Pierson <rachel.pierson@hotmail.co.uk>
2013-07-04 19:22:31 +0100

    ConsoleAppUI begun

---
+++ RPPBranchedMigrator/ConsoleAppUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using MigrationClasses;
+
+namespace BranchedMigrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Debugger.Break();
+            Dictionary<string, string> argsDict = new Dictionary<string, string>();
+
+            foreach (string str in args)
+            {
+                string[] split = str.Split('=');
+                argsDict.Add(split[0].ToString().ToLower(), split[1].ToString());
+            }
+
+            string connectionString;
+            if (argsDict.TryGetValue("connectionstring", out connectionString))
+                MigrationManager.ConnectionString = connectionString;
+            else
+                throw new Exception("No ConnectionString passed");
+        }
+    }
+
+    public class Migrate
+    {
+        public Migrate(string ConnectionString, DateTime MigrateToDate)
+        {
+
+        }
+    }
+}
